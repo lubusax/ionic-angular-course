4876cc66c0a8d632139ea68728668f02f0c6b3d5
lubusax <lu.bu.sax@google.com>
2021-05-23 15:57:52 +0200

    auto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import { UniqueDeviceID } from '@ionic-native/unique-device-id/ngx';
+import { Uid } from '@ionic-native/uid/ngx';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +21,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    UniqueDeviceID,
+    Uid,
+    AndroidPermissions,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
+import { UniqueDeviceID } from '@ionic-native/unique-device-id/ngx';
+// import { Uid } from '@ionic-native/uid/ngx';
+// import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
@@ -10,11 +14,33 @@ import { PlacesService } from '../places.service';
 export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
+  id: any;
 
-  constructor(private placesService: PlacesService) { }
+  constructor(
+    private placesService: PlacesService,
+    private uniqueDeviceID: UniqueDeviceID
+    // private uid: Uid,
+    // private androidPermissions: AndroidPermissions
+    ) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.getUniqueDeviceID();
+    // this.uniqueDeviceID.get()
+    //   .then((uuid: any) => console.log(uuid))
+    //   .catch((error: any) => console.log(error));
+  }
+
+  getUniqueDeviceID() {
+    this.uniqueDeviceID.get()
+      .then((uuid: any) => {
+        console.log(uuid);
+        this.id = uuid;
+      })
+      .catch((error: any) => {
+        console.log(error);
+        this.id = "Error! ${error}";
+      });
   }
 
 }
